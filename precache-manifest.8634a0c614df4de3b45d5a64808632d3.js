2c82b3e231cd56e78ff2b4efddc74cc1e9d536ee
linhle <linhle@Hieus-MacBook-Air.local>
2020-07-28 20:53:21 +0800

    Updates

---
+++ precache-manifest.8634a0c614df4de3b45d5a64808632d3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ef0b88055cc264f179f9a237acb8c36f",
+    "url": "/demo-dating-app-frontend/index.html"
+  },
+  {
+    "revision": "432b183f581472917524",
+    "url": "/demo-dating-app-frontend/static/css/main.5a128b9e.chunk.css"
+  },
+  {
+    "revision": "9bea114c2795d4c97317",
+    "url": "/demo-dating-app-frontend/static/js/2.07c5af51.chunk.js"
+  },
+  {
+    "revision": "5158a2014b22954f68e62461bc76a3a0",
+    "url": "/demo-dating-app-frontend/static/js/2.07c5af51.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "432b183f581472917524",
+    "url": "/demo-dating-app-frontend/static/js/main.d653110e.chunk.js"
+  },
+  {
+    "revision": "757eb7ae6e2f492f0085",
+    "url": "/demo-dating-app-frontend/static/js/runtime-main.0a86c614.js"
+  }
+]);
